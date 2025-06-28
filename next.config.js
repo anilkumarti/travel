d954f245f77d0de82ef78eff2aa9ba1e63e5fa28
Anil Tiwari <aniltiwari@MacBookAir.lan>
2025-06-28 10:07:42 +0530

    fist commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Only include valid Next.js options here
+  reactStrictMode: true,
+  // Remove swcMinify if causing issues (it's enabled by default in newer versions)
+}
 
 module.exports = nextConfig
