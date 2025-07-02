86c47421b7d2fb497561ee5b6bd8d80379930dae
Anil Tiwari <aniltiwari@MacBookAir.lan>
2025-07-02 18:04:26 +0530

    hero section added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,8 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          pattern: "url('/pattern.png')",
+          'pattern-2': "url('/pattern-bg.png')",
       },
     },
   },
